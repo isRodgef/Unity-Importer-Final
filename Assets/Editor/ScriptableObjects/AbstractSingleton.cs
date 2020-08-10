ad5a3c15878645b9fb7feac0954f79ea9d61f979
Rodger Francis <rodger.francis@snode.com>
2020-08-10 19:48:59 +0200

    Adding Singleton config for editor settings

---
+++ Assets/Editor/ScriptableObjects/AbstractSingleton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public abstract class AbstractSingleton<T> : ScriptableObject where T: ScriptableObject
+{
+    private static T objInstance  = null;
+
+    public static T Instance
+    {
+        get
+        {
+            if (!objInstance)
+            {
+                objInstance = Resources.FindObjectsOfTypeAll<T>().FirstOrDefault();
+            }
+
+            return objInstance; 
+
+        }
+        
+    }
+    
+}
---
+++ Assets/Editor/ScriptableObjects/ImportToolConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ImportToolConfig", menuName = "ScriptableObjects/ImporterToolConfig", order = 1)]
+public class ImportToolConfig : AbstractSingleton<ImportToolConfig>
+{
+    
+    // Start is called before the first frame update
+    
+    
+}
