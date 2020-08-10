d0c895f19eed214d75440f31a270f84449571291
Rodger Francis <rodger.francis@snode.com>
2020-08-10 21:57:39 +0200

    added audio

---
+++ Assets/Editor/Importers/AudioModifier.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AudioModifier : AssetImporter
+{
+    private void OnPreprocessAudio()
+    {
+        Debug.Log("Rodger does audio\n\n\n");
+    }
+
+    private void OnPostprocessAudio(AudioClip arg)
+    {
+      /*  var importer = assetImporter as AudioImporter;
+
+        if (importer == null)
+            return;
+
+
+        AudioImporterSampleSettings sampleSettings = importer.defaultSampleSettings;
+
+
+        sampleSettings.loadType = AudioClipLoadType.Streaming;
+        sampleSettings.compressionFormat = AudioCompressionFormat.AAC;
+
+        importer.defaultSampleSettings = sampleSettings;
+
+
+
+        Debug.Log(arg);*/
+    }
+}
